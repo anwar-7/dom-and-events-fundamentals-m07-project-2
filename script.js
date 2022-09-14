1dcbb353090cbbc9783191aa20f0706de8a588a8
anwarul1 <anwarjewel007@gmail.com>
2022-09-14 12:50:16 +0600

    project 2 mack a modal window active

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModel = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModel);
+
+const openModel = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModel.length; i++)
+  btnsOpenModel[i].addEventListener('click', openModel);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
